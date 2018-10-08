5dd3be0ba8280fa485a49960bde24a54d11004d5
mtavz <passion.rbit@gmail.com>
2018-10-08 09:25:04 +0700

    add contact

---
+++ CozaStore/Controllers/ContactController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CozaStore.Controllers
+{
+    public class ContactController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
